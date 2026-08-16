5ebffc8408c7db96d06260c4ed5e982d2a50ea5f
LEE-wz <leeweizhi2217@gmail.com>
2026-08-16 22:04:34 +0800

    Add greeting and exit messages

--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -1,7 +1,18 @@
 public class Remy {
     public static void main(String[] args) {
-        String banner =
-                  " _____                      \n"
+        String banner = getBannerMessage();
+        String greetMsg = getGreetMessage();
+        String exitMsg = getExitMessage();
+
+        System.out.println(banner);
+        System.out.println(greetMsg);
+        System.out.println(exitMsg);
+    }
+
+    public static String getBannerMessage() {
+        return
+                  "____________________________________________________________\n"
+                + " _____                      \n"
                 + "|  __ \\                     \n"
                 + "| |__) | ___ _ __ ___  _   _\n"
                 + "|  _  / / _ \\ '_ ` _ \\| | | |\n"
@@ -9,6 +20,19 @@ public class Remy {
                 + "|_|  \\_\\\\___|_| |_| |_|\\__, |\n"
                 + "                        __/ |\n"
                 + "                       |___/ \n";
-        System.out.println(banner);
+    }
+
+    public static String getGreetMessage() {
+        return
+              "Hello! I'm Remy.\n"
+            + "What can I do for you?\n"
+            + "\n"
+            + "____________________________________________________________";
+    }
+
+    public static String getExitMessage() {
+        return
+                  "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
     }
 }
