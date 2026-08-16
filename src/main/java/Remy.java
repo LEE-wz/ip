7b5bcedafb2bfee147a6288220414de75b90b0eb
LEE-wz <leeweizhi2217@gmail.com>
2026-08-16 21:48:03 +0800

    Change chatbot name from 'Duke' to 'Remy'

---
+++ src/main/java/Remy.java
@@ -0,0 +1,14 @@
+public class Remy {
+    public static void main(String[] args) {
+        String banner =
+                  " _____                      \n"
+                + "|  __ \\                     \n"
+                + "| |__) | ___ _ __ ___  _   _\n"
+                + "|  _  / / _ \\ '_ ` _ \\| | | |\n"
+                + "| | \\ \\|  __/ | | | | | |_| |\n"
+                + "|_|  \\_\\\\___|_| |_| |_|\\__, |\n"
+                + "                        __/ |\n"
+                + "                       |___/ \n";
+        System.out.println(banner);
+    }
+}
