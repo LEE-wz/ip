8d0ed41bda8514b783a62f6bfeb66ea86130ebf7
LEE-wz <leeweizhi2217@gmail.com>
2026-08-26 08:22:22 +0800

    Delete original files

--- src/main/java/ListCommand.java
+++
@@ -1,9 +0,0 @@
-/**
- * Displays every task in the task list.
- */
-public class ListCommand extends Command {
-    @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage) {
-        ui.showTaskList(tasks.getTasks());
-    }
-}
