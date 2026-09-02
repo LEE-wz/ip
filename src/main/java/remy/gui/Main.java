85d18fc3d790afe7f5e7e0205a1e4aa7bc3755c7
LEE-wz <leeweizhi2217@gmail.com>
2026-09-02 12:14:43 +0800

    Add header comments

--- src/main/java/remy/gui/Main.java
+++ src/main/java/remy/gui/Main.java
@@ -5,6 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+/**
+ * GUI class Main that extends Application
+ */
 public class Main extends Application {
 
     @Override
